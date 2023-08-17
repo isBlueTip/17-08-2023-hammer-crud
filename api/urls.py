6d1d94e367dfddb8fed6e0aa60a3e0da38017818
Simon Egorov <rhinorofl@gmail.com>
2023-08-17 23:32:33 +0700

    Create 'api' app

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,3 @@
-from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 
@@ -8,5 +7,7 @@ router = routers.DefaultRouter()
 router.register("users", views.CurrentClientViewSet, basename="user")
 
 urlpatterns = [
-    include("v1/", namespace="api"),
+    path("v1/", include(router.urls)),
 ]
+
+app_name = 'users'
