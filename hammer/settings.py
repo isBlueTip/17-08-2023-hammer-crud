7a4150a51f7347b27f1a61948b2a146bd177a4b9
Simon Egorov <rhinorofl@gmail.com>
2023-08-17 23:31:17 +0700

    Create 'users' migrations

--- hammer/settings.py
+++ hammer/settings.py
@@ -108,3 +108,4 @@ STATIC_URL = "static/"
 # Default primary key field type
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+AUTH_USER_MODEL = "users.User"
--- users/models.py
+++ users/models.py
@@ -1,3 +1,22 @@
+import time
+from random import choices
+
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    phone_number = models.CharField(max_length=15, unique=True)
+    auth_code = models.CharField(max_length=4)
+    invite_code = models.CharField(max_length=6, unique=True, null=True)
+    referred_by = models.ForeignKey("self", null=True, on_delete=models.SET_NULL)
+
+    USERNAME_FIELD = "phone_number"
+
+    def __str__(self):
+        return f"{self.username} | {self.phone_number}"
+
+    @staticmethod
+    def generate_otp():
+        time.sleep(2)
+        return "".join(choices("0123456789", k=4))
