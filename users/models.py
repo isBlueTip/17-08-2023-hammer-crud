2d08c7c1a24f25686f68d91791ae2f123d60578c
Simon Egorov <rhinorofl@gmail.com>
2023-08-18 20:34:56 +0700

    Update 'users' models

--- users/models.py
+++ users/models.py
@@ -1,13 +1,14 @@
-import time
-from random import choices
-
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+
+from .services import generate_invite_code, validate_phone
 
 
 class User(AbstractUser):
-    phone_number = models.CharField(max_length=15, unique=True)
-    auth_code = models.CharField(max_length=4)
+    phone_number = models.CharField(max_length=15, unique=True, validators=(validate_phone,))
+    otp = models.CharField(max_length=4)
     invite_code = models.CharField(max_length=6, unique=True, null=True)
     referred_by = models.ForeignKey("self", null=True, on_delete=models.SET_NULL)
 
@@ -16,7 +17,12 @@ class User(AbstractUser):
     def __str__(self):
         return f"{self.username} | {self.phone_number}"
 
-    @staticmethod
-    def generate_otp():
-        time.sleep(2)
-        return "".join(choices("0123456789", k=4))
+    @receiver(pre_save)
+    def populate_invite_code(sender, instance, *args, **kwargs):
+        if instance.invite_code:
+            return
+        while True:
+            invite_code = generate_invite_code()
+            if not User.objects.filter(invite_code=invite_code).exists():
+                break
+        instance.invite_code = invite_code
