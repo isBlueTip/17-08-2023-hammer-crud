24f0df265e576c0eb471e66e738031f8677e2cb6
Simon Egorov <rhinorofl@gmail.com>
2023-08-18 20:37:30 +0700

    Update 'users' serializers

--- users/serializers.py
+++ users/serializers.py
@@ -1,25 +1,12 @@
 from rest_framework import serializers
 
 from .models import User
+from .services.validators import validate_otp, validate_phone
 
 
 class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = (
-            "username",
-            "phone_number",
-            "email",
-            "is_active",
-            "referee",  # как назвать людей, кого привёл?
-        )
-        # read_only_fields = (
-        #     ''
-        # )
-
-
-class CreateUserSerializer(serializers.ModelSerializer):
-    otp = serializers.IntegerField()
+    otp = serializers.CharField(max_length=4, min_length=4, validators=(validate_otp,), write_only=True, required=False)
+    referred_by = serializers.StringRelatedField(read_only=True)
 
     class Meta:
         model = User
@@ -28,5 +15,11 @@ class CreateUserSerializer(serializers.ModelSerializer):
             "phone_number",
             "email",
             "otp",
+            "referred_by",
+            # "referrer",  # как назвать людей, кого привёл?
         )
-        read_only_fields = ""
+
+        extra_kwargs = {
+            "username": {"validators": ()},  # Exclude username from uniqueness validation
+            "phone_number": {"validators": (validate_phone,)},  # Exclude username from uniqueness validation
+        }
