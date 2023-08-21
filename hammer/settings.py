6a450e7fe3265228be683e44872a2b506ded62b4
Simon Egorov <rhinorofl@gmail.com>
2023-08-21 17:27:55 +0700

    Update 'settings.py'

--- hammer/settings.py
+++ hammer/settings.py
@@ -2,11 +2,11 @@ import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-SECRET_KEY = "django-insecure-5fguvbloe*_8ty-p-ow0mf5^zd_x1wul=w_svns)na72^+4kk6"
+SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-5fguvbloe*_8ty-p-ow0mf5^zd_x1wul=w_svns)na72^+4kk6")
 
-DEBUG = True
+DEBUG = os.getenv("DEBUG", True)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -21,10 +21,12 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
 
     # 3rd party
+    "rest_framework",
+    "rest_framework.authtoken",
 
     # Own
-    "api",
-    "users"
+    "api.apps.ApiConfig",
+    "users.apps.UsersConfig"
 ]
 
 MIDDLEWARE = [
@@ -61,14 +63,27 @@ WSGI_APPLICATION = "hammer.wsgi.application"
 # Database
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME', 'postgres'),
-        'USER': os.getenv('DB_USER', 'postgres'),
-        'PASSWORD': os.getenv('DB_PASSWORD', 'postgres'),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', '5432'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME", "postgres"),
+        "USER": os.getenv("DB_USER", "postgres"),
+        "PASSWORD": os.getenv("DB_PASSWORD", "postgres"),
+        "HOST": os.getenv("DB_HOST", "localhost"),
+        "PORT": os.getenv("DB_PORT", "5432"),
     }
+}
+
+
+# DRF
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated'
+    ),
 }
 
 
